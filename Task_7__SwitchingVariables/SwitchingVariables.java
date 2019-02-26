0c7ba619e4cf4b023ea356df2acec848fa542e77
Aliaksandr Rasolka <47742350+MyFirstMorningCupOfCoffee@users.noreply.github.com>
2019-02-26 14:48:52 +0300

    Create SwitchingVariables.java

---
+++ Task_7__SwitchingVariables/SwitchingVariables.java
@@ -0,0 +1,55 @@
+import static io.OutputWorker.emptyLine;
+import static io.OutputWorker.printlnToConsole;
+
+public class SwitchingVariables 
+{
+    public static void main(String[] args)
+    {
+        showBriefing();
+        
+        int a, b;
+        
+        // 1st
+        a = 985;
+        b = -1021;
+        printlnToConsole(":::part 1:::", "before:");
+        demonstrateVariables(a, b);
+        
+        a += b;
+        b = a - b;
+        a = a - b;
+        printlnToConsole("after:");
+        demonstrateVariables(a, b);
+        
+        emptyLine();
+        
+        // 2nd
+        a = 11987;
+        b = -3;
+        printlnToConsole(":::part 2:::", "before:");
+        demonstrateVariables(a, b);
+        
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        printlnToConsole("after:");
+        demonstrateVariables(a, b);
+        
+    }
+    
+    
+    private static void showBriefing()
+    {
+        printlnToConsole(
+            "Greetings!", 
+            "This application shows how 2 integer value can be switched without" + 
+                    " creating additional variables.");
+
+            emptyLine();
+    }
+    
+    private static void demonstrateVariables(int a, int b)
+    {
+        printlnToConsole("a = " + a + ", b = " + b);
+    }
+}
