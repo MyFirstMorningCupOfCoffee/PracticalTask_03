f2ed79fff4a310ef809c4aefe249ef2c6d066112
Aliaksandr Rasolka <47742350+MyFirstMorningCupOfCoffee@users.noreply.github.com>
2019-02-26 09:44:38 +0300

    Create InputWorker.java

---
+++ Task_1__WeighingOurDino/io/InputWorker.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class InputWorker 
+{
+    
+    private InputWorker()
+    { }
+   
+    
+    public static String readStringFromConsole()
+    {
+        return new Scanner(System.in).nextLine();
+    }
+    
+    
+    public static Double readDoubleFromConsole()
+    {
+        String src = readStringFromConsole();
+        
+        /// bypass delimeter locales problems
+        src = src.replaceFirst(",", ".");
+        Scanner sc = new Scanner(src);
+        sc.useLocale(Locale.UK);
+        /// /// /// /// /// /// /// ///
+        
+        return sc.nextDouble();  
+    }
+    
+}
